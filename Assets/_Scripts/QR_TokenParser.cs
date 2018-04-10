1e813b7f3d6b6e990ec28307256c747ed0c4d54f
MindaugasVainauskas <g00327967@gmit.ie>
2018-04-11 00:16:05 +0100

    Added Unity network call to pull in Graph data over network.

--- Assets/_Scripts/QR_TokenParser.cs
+++ Assets/_Scripts/QR_TokenParser.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using ZXing;
 using ZXing.Client.Result;
 
@@ -9,11 +10,15 @@ public class QR_TokenParser : MonoBehaviour {
     private WebCamTexture camTexture;
     private Rect screenRect;
     private string decodedString;
+    private GraphAccessToken token;
 
     // Use this for initialization
     void Start ()
     {
+        //set decoded string to null
         decodedString = null;
+        //instantiate new GraphAccessToken class
+        token = new GraphAccessToken();
         screenRect = new Rect(0, 0, Screen.width, Screen.height);
         camTexture = new WebCamTexture();
         camTexture.requestedHeight = Screen.height;
@@ -37,17 +42,41 @@ public class QR_TokenParser : MonoBehaviour {
               camTexture.width, camTexture.height);
             if (result != null && decodedString == null)
             {
+                //parse QR code result with ZXing result parser class
                 var resSon = ResultParser.parseResult(result);
-                string jsonstring = resSon.ToString();
-                //Debug.Log(jsonstring);
-                GraphAccessToken token = new GraphAccessToken();
-                token = (GraphAccessToken)JsonUtility.FromJson(jsonstring, typeof(GraphAccessToken));
+                //set it to string
+                decodedString = resSon.ToString();
+                //Use Unity built in JsonUtility.FromJson() method to deserialize the string
+                token = (GraphAccessToken)JsonUtility.FromJson(decodedString, typeof(GraphAccessToken));
+
+                //check for output.
                 Debug.Log(token.url);
                 Debug.Log(token.auth_token);
-                // decodedString = result.Text;
-                //Debug.Log("DECODED TEXT FROM QR: " +result.Text);
+
+                //send request to get full JSON.
+                StartCoroutine(GetTheJson(token));
             }
+            
+                
         }
         catch (Exception ex) { Debug.LogWarning(ex.Message); }
+    }
+
+    IEnumerator GetTheJson(GraphAccessToken token)
+    {
+        UnityWebRequest jsonRequest = UnityWebRequest.Get("http://"+token.url);
+        jsonRequest.SetRequestHeader("x-hololens-token", token.auth_token);
+        yield return jsonRequest.Send();
+        Debug.Log("Request sent with auth_token: "+token.auth_token);
+
+        if (jsonRequest.isError)
+        {
+            Debug.Log(jsonRequest.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(jsonRequest.downloadHandler.text);
+        }
     }
 }
