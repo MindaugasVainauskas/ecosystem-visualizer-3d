74b9924b5c4c33428fc951107951044516bb96cd
MindaugasVainauskas <g00327967@gmit.ie>
2018-04-12 19:14:29 +0100

    Added NodeManager class to handle graph node related code. Moved graph node related code out of Graph loader class.

--- Assets/_Scripts/GraphLoader.cs
+++ Assets/_Scripts/GraphLoader.cs
@@ -8,14 +8,15 @@ public class GraphLoader : MonoBehaviour
 {
     //Declaration for Relationship manager class, responsible for drawing the relationships
     private RelationshipManager relManager;
-    public List<GameObject> objectList;
+    private NodeManager nodeManager;
+    private List<GameObject> nodeList;
     public List<Cell_Link> relations;
     private float _graphScale = 5;
     public GameObject ParentObject; //Parent object holding graph objects inside it.
     private Vector3 startPos;
     private Vector3 endPos;
     
-    //Declarations of transforms for gameobjects that are mapped to certain 3d model prefab.
+    //Declarations of transforms for gameobjects that are mapped to Unity3d prefabs.
     public Transform cloudService;
     public Transform cube;
     public Transform diamond;
@@ -30,7 +31,12 @@ public class GraphLoader : MonoBehaviour
     //gets called before start. Called once per object lifetime
     void Awake()
     {
+        //Relationship manager class gets instantiated with the current graph scale value.
         relManager = new RelationshipManager(_graphScale);
+        //Instantiate Node drawing class
+        nodeManager = new NodeManager();
+        //Instantiate local node list, which will pull its value from nodemanager getObjectlist() method when needed
+        nodeList = new List<GameObject>();
         //Load the JSON received through GET request in QR code reader class.
         string data = PlayerPrefs.GetString("JSON_graph_data");
         //Load the graph from given data.
@@ -41,7 +47,6 @@ public class GraphLoader : MonoBehaviour
     public void LoadGraph(string path)
     {
         ParentObject = GameObject.Find("ParentObject");
-        objectList = new List<GameObject>();
         string loadGraph = path;
 
         relations = new List<Cell_Link>();
@@ -49,8 +54,7 @@ public class GraphLoader : MonoBehaviour
         JSONNode jNode = JSON.Parse(loadGraph);
 
         //split JSON node into JSON array. Had to add ["graph"] at start to handle QR code input
-        JSONArray cellArray = (JSONArray)jNode["graph"]["cells"];
-        
+        JSONArray cellArray = (JSONArray)jNode["graph"]["cells"];        
 
         foreach (JSONNode cell in cellArray)
         {
@@ -69,20 +73,20 @@ public class GraphLoader : MonoBehaviour
                     switch (objectShape)
                     {
                         case "basic.InteractiveCircle":
-                            instantiateObject(cloudService, posX, posZ, objId);
+                            nodeManager.instantiateObject(cloudService, posX, posZ, objId);
                             break;
                         case "basic.InteractiveRect":
-                            instantiateObject(cube, posX, posZ, objId);
+                            nodeManager.instantiateObject(cube, posX, posZ, objId);
                             break;
                         case "basic.InteractiveDiamond":
-                            instantiateObject(diamond, posX, posZ, objId);
+                            nodeManager.instantiateObject(diamond, posX, posZ, objId);
                             break;
                         case "basic.InteractiveHex":
-                            instantiateObject(hex3d, posX, posZ, objId);
+                            nodeManager.instantiateObject(hex3d, posX, posZ, objId);
                             break;
                         default:
                             //If the prefab is not in the list, Just instantiate it as a sphere.
-                            instantiateObject(sphere, posX, posZ, objId);
+                            nodeManager.instantiateObject(sphere, posX, posZ, objId);
                             break;
                     }                    
                     break;
@@ -111,21 +115,26 @@ public class GraphLoader : MonoBehaviour
             }//end of switch(cellType)
         }//end of forEach(JSONNode cell in cellArray)
 
+        //Assign nodeManager object list to the local node list collection.
+        nodeList = nodeManager.getObjectList();
+
+        //Each link will need to be drawn with data from relations list and start/end positions from object list in nodeList list
         foreach (var link in relations)
         {
             //create 2 vector3 objects to hold start and end positions for every relationship
             startPos = new Vector3();
-            endPos = new Vector3();
-            
-            for (int i = 0; i < objectList.Count; i++)
+            endPos = new Vector3();           
+
+            //Check for start and end positions
+            for (int i = 0; i < nodeList.Count; i++)
             {
-                if(link.sourceId.Equals(objectList[i].name))
+                if(link.sourceId.Equals(nodeList[i].name))
                 {
-                    startPos = objectList[i].transform.localPosition;
+                    startPos = nodeList[i].transform.localPosition;
                 }
-                else if (link.targetId.Equals(objectList[i].name))
+                else if (link.targetId.Equals(nodeList[i].name))
                 {
-                    endPos = objectList[i].transform.localPosition;
+                    endPos = nodeList[i].transform.localPosition;
                 }
                 else
                 {
@@ -136,15 +145,4 @@ public class GraphLoader : MonoBehaviour
             relManager.drawRelationship(startPos, endPos, link.relCol, link.relText);
         }//end of foreach method
     }//end of LoadGraph method
-
-    //this is an reusable method to instantiate transform objects from prefabs.
-    void instantiateObject(Transform shape, float posX, float posZ, string objId) {
-        float scaleFactor = 0.09f;
-        float distanceReducer = 30f;
-        Transform clone = Instantiate(shape, new Vector3(posX/ distanceReducer, -4.6f, ((posZ/ distanceReducer)+10)), Quaternion.identity);
-        clone.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
-        clone.name = "" + objId;
-        clone.parent = ParentObject.transform;
-        objectList.Add(clone.gameObject);
-    }  
 }
---
+++ Assets/_Scripts/NodeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager{
+    
+    public GameObject parentObject; //Parent object holding graph objects inside it.
+    private List<GameObject> objectList;
+
+    //Constructor
+    public NodeManager()
+    {
+        parentObject = GameObject.Find("ParentObject");
+        objectList = new List<GameObject>();
+    }
+
+    //Reusable graph node creation method
+    public void instantiateObject(Transform shape, float posX, float posZ, string objId)
+    {
+        float scaleFactor = 0.09f;
+        float distanceReducer = 30f;
+        Transform clone = Object.Instantiate(shape, new Vector3(posX / distanceReducer, -4.6f, ((posZ / distanceReducer) + 10)), Quaternion.identity);
+        clone.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+        clone.name = "" + objId;
+        clone.parent = parentObject.transform;
+        objectList.Add(clone.gameObject);
+    }
+
+    public List<GameObject> getObjectList()
+    {
+        return objectList;
+    }
+
+}
